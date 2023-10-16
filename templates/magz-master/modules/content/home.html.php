510740019cdeb3d2e8235cd65e4ba07f246dbeeb
ibrahimbtaz <ibrahimbtaz@gmail.com>
2023-10-16 14:56:06 +0700

    a

--- templates/magz-master/modules/content/home.html.php
+++ templates/magz-master/modules/content/home.html.php
@@ -1,36 +1,84 @@
 <?php if (!defined('_VALID_BBC')) exit('No direct script access allowed');
 
 $config = $output['config'];
-$arr    = $output['data'];
-foreach ((array) $arr as $data) {
-  // pr($data);
-  // pr($config);
+$arr		=  $output['data'];
 ?>
-  <article class="featured">
-    <div class="overlay"></div>
-    <figure>
-      <?php echo (!empty($config['thumbnail']) && !empty($data['image'])) ? content_src($data['image'], ' class="" title="' . $data['title'] . '"') : ''; ?>
-      <!-- <img src="/surya/templates/magz-master/images/news/img04.jpg" alt="Sample Article"> -->
-    </figure>
-    <div class="details">
-      <?php
-      $link = content_link($data['id'], $data['title']);
-      if ($config['title']) {
-        if ($config['title_link']) {
-      ?>
-          <!-- <div class="category"><a href="category.html">Computer</a></div> -->
-          <div class="category"><a href="<?php echo $link; ?>" title="<?php echo $data['title']; ?>"><?php echo $data['title']; ?></a></div>
-        <?php
-        } else {
-        ?>
-          <div class="category"><?php echo $data['title']; ?></div>
-      <?php
-        }
-      }
-      ?>
-      <h1><a href="single.html"> <?php echo substr($data['content'], 0, 80) ?></a></h1>
-      <div class="time"><?php echo $data['created'] ?></div>
-    </div>
-  </article>
-<?php
-}
+<div class="item">
+	<?php
+	foreach ((array)$arr as $data) {
+	?>
+		<article class="featured">
+			<div class="overlay"></div>
+
+			<figure>
+				<?php echo (!empty($config['thumbnail']) && !empty($data['image'])) ? content_src($data['image'], ' class="" title="' . $data['title'] . '"') : ''; ?>
+			</figure>
+			<div class="details">
+				<?php
+				if ($config['tag']) {
+					$r = content_category($data['id'], $config['tag_link']);
+				?>
+					<div class="category text-light"><?php echo lang('Tags') . implode(' ', $r) ?></div>
+				<?php
+				}
+				?>
+				<?php
+				if ($config['author']) {
+				?>
+					<div class="category"><a href="<?php echo $link; ?>" title="<?php echo $data['title']; ?>" class=""><?php echo $data['created_by_alias']; ?></a></div>
+				<?php
+				}
+				$link = content_link($data['id'], $data['title']);
+				if ($config['title']) {
+					if ($config['title_link']) {
+					?>
+						<h1><a href="<?php echo $link; ?>" title="<?php echo $data['title']; ?>"><?php echo $data['title'] ?></a></h1>
+					<?php
+					} else {
+					?>
+						<h1><?php echo $data['title'] ?></h1>
+					<?php
+					}
+				}
+				if ($config['created']) {
+					?>
+					<!-- <div class="category"><a href="category.html">Computer</a></div> -->
+					<div class="text-light time">
+						<?php echo ($config['created']) ?  content_date($data['created'])  : ''; ?>
+					</div>
+				<?php
+				}
+				?>
+				<?php
+				if ($config['rating'] || $config['modified']) {
+				?>
+					<div class="time">
+						<?php
+						if ($config['rating']) {
+						?>
+							<div class="col-md-6 no-both text-light">
+								<?php echo rating($data['rating']); ?>
+							</div>
+						<?php
+						}
+						if (empty($data['revised'])) {
+							$config['modified'] = 0;
+						}
+						if (!empty($config['modified'])) {
+						?>
+							<div class="col-md-6 no-left text-right">
+								<em class="text-right pull-right text-light"><?php echo lang('modified') . content_date($data['modified']); ?></em>
+							</div>
+						<?php
+						}
+						?>
+						<div class="clearfix"></div>
+					</div>
+				<?php
+				}
+				?>
+		</article>
+	<?php
+	}
+	?>
+</div>
